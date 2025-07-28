abdf415ba81b690fc1e3bd8cf8233dae40e49273
SimpleFreak <andreyrazinwork@yandex.ru>
2025-07-29 01:43:44 +0300

    Modified files

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,1 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+
+from backend.core.config import settings
+
+
+engine = create_engine(
+    settings.DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_database():
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from core.config import settings
+
+app = FastAPI(
+    title="Interactive story generator",
+    description="Api to generate cool stories",
+    version="1.0.0",
+    docs_url="/redoc"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run("main.py", host="0.0.0.0", port=1000, reload=True)
